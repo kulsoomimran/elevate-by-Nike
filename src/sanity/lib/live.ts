bb8793621a8c9c70cdaf1bebcac905934cd133d6
Kulsoom Imran <kulsoomimran1901@gmail.com>
2026-04-12 00:11:28 +0500

    fix issues

--- src/sanity/lib/live.ts
+++ src/sanity/lib/live.ts
@@ -2,12 +2,19 @@
 // Before using it, import and render "<SanityLive />" in your layout, see
 // https://github.com/sanity-io/next-sanity#live-content-api for more information.
 import { defineLive } from "next-sanity";
-import client  from './client'
+import { createClient } from "next-sanity";
+
+const client = createClient({
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
+  apiVersion: '2021-08-31',
+  useCdn: false,
+  stega: {
+    enabled: false,
+    studioUrl: '/studio',
+  },
+});
 
 export const { sanityFetch, SanityLive } = defineLive({ 
-  client: client.withConfig({ 
-    // Live content is currently only available on the experimental API
-    // https://www.sanity.io/docs/api-versioning
-    apiVersion: '2021-08-31'
-  }) 
+  client
 });
